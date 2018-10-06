79fa881795471ffd03d5dbbd672f2c198326c7f3
bartchr808 <bart.chr@gmail.com>
2018-10-06 01:17:17 -0700

    Rectangle cropper.

--- Codeigma/ImageCropperViewController.swift
+++ Codeigma/ImageCropperViewController.swift
@@ -8,8 +8,17 @@
 
 import UIKit
 
-class ImageCropperViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ImageCropperViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UIScrollViewDelegate {
     @IBOutlet weak var imagePicked: UIImageView!
+    @IBOutlet weak var cropAreaView: CropAreaView!
+
+    @IBOutlet var scrollView: UIScrollView!{
+        didSet{
+            scrollView.delegate = self
+            scrollView.minimumZoomScale = 1.0
+            scrollView.maximumZoomScale = 10.0
+        }
+    }
 
     @IBAction func openCameraButton(sender: AnyObject) {
         if UIImagePickerController.isSourceTypeAvailable(.camera) {
@@ -31,5 +40,55 @@ class ImageCropperViewController: UIViewController, UIImagePickerControllerDeleg
         }
         imagePicked.image = image
         dismiss(animated:true, completion: nil)
+    }
+
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imagePicked
+    }
+
+    var cropArea:CGRect{
+        get{
+            let factor = imagePicked.image!.size.width/view.frame.width
+            let scale = 1/scrollView.zoomScale
+            let imageFrame = imagePicked.imageFrame()
+            let x = (scrollView.contentOffset.x + cropAreaView.frame.origin.x - imageFrame.origin.x) * scale * factor
+            let y = (scrollView.contentOffset.y + cropAreaView.frame.origin.y - imageFrame.origin.y) * scale * factor
+            let width = cropAreaView.frame.size.width * scale * factor
+            let height = cropAreaView.frame.size.height * scale * factor
+            return CGRect(x: x, y: y, width: width, height: height)
+        }
+    }
+
+    @IBAction func crop(_ sender: UIButton) {
+        let croppedCGImage = imagePicked.image?.cgImage?.cropping(to: cropArea)
+        let croppedImage = UIImage(cgImage: croppedCGImage!)
+        imagePicked.image = croppedImage
+
+        scrollView.zoomScale = 1
+    }
+}
+
+extension UIImageView {
+    func imageFrame()->CGRect{
+        let imageViewSize = self.frame.size
+
+        guard let imageSize = self.image?.size else{return CGRect.zero}
+
+        let imageRatio = imageSize.width / imageSize.height
+        let imageViewRatio = imageViewSize.width / imageViewSize.height
+
+        if imageRatio < imageViewRatio {
+            let scaleFactor = imageViewSize.height / imageSize.height
+            let width = imageSize.width * scaleFactor
+            let topLeftX = (imageViewSize.width - width) * 0.5
+
+            return CGRect(x: topLeftX, y: 0, width: width, height: imageViewSize.height)
+        } else {
+            let scalFactor = imageViewSize.width / imageSize.width
+            let height = imageSize.height * scalFactor
+            let topLeftY = (imageViewSize.height - height) * 0.5
+
+            return CGRect(x: 0, y: topLeftY, width: imageViewSize.width, height: height)
+        }
     }
 }

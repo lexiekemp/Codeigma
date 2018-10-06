55caca0b357747abf0a230413724773e7032e5e1
bartchr808 <bart.chr@gmail.com>
2018-10-05 23:12:43 -0700

    temp

--- Codeigma/Constants.swift
+++ Codeigma/Constants.swift
@@ -8,5 +8,5 @@
 
 import UIKit
 
-let codeigmaDarkBlue = UIColor(red: 31, green: 20, blue: 159, alpha: 1)
-let codeigmaLightBlue = UIColor(red: 101, green: 235, blue: 244, alpha: 1)
+let codeigmaDarkBlue = UIColor(red: 31/255, green: 20/255, blue: 159/255, alpha: 1)
+let codeigmaLightBlue = UIColor(red: 101/255, green: 235/255, blue: 244/255, alpha: 1)
--- Codeigma/ViewController.swift
+++ Codeigma/ViewController.swift
@@ -10,31 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let gradientLayer = CAGradientLayer()
+    @IBOutlet weak var logo: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // 1
-        self.view.backgroundColor = UIColor.green
+        let gradientLayer = CAGradientLayer()
 
-        // 2
         gradientLayer.frame = self.view.bounds
 
-        // 3
-        let color1 = UIColor.yellow.cgColor as CGColor
-        let color2 = UIColor(red: 1.0, green: 0, blue: 0, alpha: 1.0).cgColor as CGColor
-        let color3 = UIColor.clear.cgColor as CGColor
-        let color4 = UIColor(white: 0.0, alpha: 0.7).cgColor as CGColor
-        gradientLayer.colors = [color1, color2, color3, color4]
+        //gradientLayer.colors = [UIColor.red.cgColor, codeigmaLightBlue.cgColor]
+        gradientLayer.colors = [codeigmaDarkBlue.cgColor, codeigmaLightBlue.cgColor]
 
-        // 4
-        gradientLayer.locations = [0.0, 0.25, 0.75, 1.0]
+        gradientLayer.locations = [0.0, 1.0]
 
-        // 5
-        self.view.layer.addSublayer(gradientLayer)
+        let container = UIView()
+
+        container.layer.addSublayer(gradientLayer)
+        self.view.addSubview(container)
+        self.view.sendSubviewToBack(container)
     }
-
-
 }
 

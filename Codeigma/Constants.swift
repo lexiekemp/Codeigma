1d64546e287f30bffa489676bd1ba73c803a0275
bartchr808 <bart.chr@gmail.com>
2018-10-05 21:18:38 -0700

    Add gradient and colors

---
+++ Codeigma/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Codeigma
+//
+//  Created by Bart Chrzaszcz on 2018-10-05.
+//  Copyright © 2018 Lexie Kemp. All rights reserved.
+//
+
+import UIKit
+
+let codeigmaDarkBlue = UIColor(red: 31, green: 20, blue: 159, alpha: 1)
+let codeigmaLightBlue = UIColor(red: 101, green: 235, blue: 244, alpha: 1)
--- Codeigma/ViewController.swift
+++ Codeigma/ViewController.swift
@@ -10,9 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let gradientLayer = CAGradientLayer()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // 1
+        self.view.backgroundColor = UIColor.green
+
+        // 2
+        gradientLayer.frame = self.view.bounds
+
+        // 3
+        let color1 = UIColor.yellow.cgColor as CGColor
+        let color2 = UIColor(red: 1.0, green: 0, blue: 0, alpha: 1.0).cgColor as CGColor
+        let color3 = UIColor.clear.cgColor as CGColor
+        let color4 = UIColor(white: 0.0, alpha: 0.7).cgColor as CGColor
+        gradientLayer.colors = [color1, color2, color3, color4]
+
+        // 4
+        gradientLayer.locations = [0.0, 0.25, 0.75, 1.0]
+
+        // 5
+        self.view.layer.addSublayer(gradientLayer)
     }
 
 
